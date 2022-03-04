e417cd28d66fd03a28ee7c4b5c8b9eef3e25cde0
bhuedies <bhuedies@gmail.com>
2022-03-04 19:48:00 +0700

    Add project files.

---
+++ WPFTemplateMVVM/ViewModels/ShellViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+namespace WPFTemplateMVVM.ViewModels;
+
+public class ShellViewModel : PropertyChangedBase
+{
+    string? name;
+
+    public string Name
+    {
+        get => name;
+        set
+        {
+            name = value;
+            NotifyOfPropertyChange(() => Name);
+            NotifyOfPropertyChange(() => CanSayHello);
+        }
+    }
+
+    public bool CanSayHello
+    {
+        get { return !string.IsNullOrWhiteSpace(Name); }
+    }
+
+    public void SayHello()
+    {
+        MessageBox.Show(string.Format("Hello {0}!", Name)); //Don't do this in real life :)
+    }
+}
+
